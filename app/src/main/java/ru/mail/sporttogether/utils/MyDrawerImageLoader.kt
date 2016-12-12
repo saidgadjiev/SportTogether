af1877dc7aec1d9cc9ed020400b431f6b1cae547
Vladislav Kozhushko <bagrusss@rocketguys.com>
2016-12-12 14:40:20 +0300

    Progress

--- app/src/main/java/ru/mail/sporttogether/utils/MyDrawerImageLoader.kt
+++ app/src/main/java/ru/mail/sporttogether/utils/MyDrawerImageLoader.kt
@@ -3,15 +3,15 @@ package ru.mail.sporttogether.utils
 import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.widget.ImageView
-//import com.bumptech.glide.Glide
+import com.bumptech.glide.Glide
 import com.mikepenz.materialdrawer.util.AbstractDrawerImageLoader
 
 class MyDrawerImageLoader : AbstractDrawerImageLoader() {
-    override fun set(imageView: ImageView?, uri: Uri?, placeholder: Drawable?) {
-//        Glide.with(imageView?.context).load(uri).placeholder(placeholder).into(imageView)
+    override fun set(imageView: ImageView, uri: Uri?, placeholder: Drawable) {
+        Glide.with(imageView.context).load(uri).placeholder(placeholder).into(imageView)
     }
 
-    override fun cancel(imageView: ImageView?) {
-//        Glide.clear(imageView)
+    override fun cancel(imageView: ImageView) {
+        Glide.clear(imageView)
     }
 }
