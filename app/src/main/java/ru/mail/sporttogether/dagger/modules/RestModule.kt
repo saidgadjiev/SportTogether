4d0e764bd6ca99ecffb53c0ee6b5dc1ed45862a7
Vladislav Kozhushko <bagrusss@rocketguys.com>
2016-12-24 20:45:29 +0300

    Dagger for service API

--- app/src/main/java/ru/mail/sporttogether/dagger/modules/RestModule.kt
+++ app/src/main/java/ru/mail/sporttogether/dagger/modules/RestModule.kt
@@ -3,35 +3,39 @@ package ru.mail.sporttogether.dagger.modules
 import dagger.Module
 import dagger.Provides
 import retrofit2.Retrofit
-import ru.mail.sporttogether.net.api.AuthorizationAPI
-import ru.mail.sporttogether.net.api.CategoriesAPI
-import ru.mail.sporttogether.net.api.EventsAPI
-import ru.mail.sporttogether.net.api.YandexMapsApi
+import ru.mail.sporttogether.net.api.*
 import javax.inject.Named
 import javax.inject.Singleton
 
 /**
- * Created by bagrusss on 30.09.16.
- *
+ * Created by bagrusss on 30.09.16
  */
 @Module
-@Singleton
 class RestModule {
 
+    @Singleton
     @Provides
     fun provideAuthApi(retrofit: Retrofit): AuthorizationAPI =
             retrofit.create(AuthorizationAPI::class.java)
 
+    @Singleton
     @Provides
     fun provideEventsApi(retrofit: Retrofit): EventsAPI =
             retrofit.create(EventsAPI::class.java)
 
+    @Singleton
     @Provides
     fun provideCategoriesApi(retrofit: Retrofit): CategoriesAPI =
             retrofit.create(CategoriesAPI::class.java)
 
+    @Singleton
     @Provides
     fun provideYandexApi(@Named("yandexmaps") retrofit: Retrofit): YandexMapsApi =
             retrofit.create(YandexMapsApi::class.java)
+
+    @Singleton
+    @Provides
+    fun provideServiceApi(retrofit: Retrofit): ServiceApi =
+            retrofit.create(ServiceApi::class.java)
 
 }
