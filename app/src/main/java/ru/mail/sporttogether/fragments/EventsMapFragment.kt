017dc9aef417b086ac81fac679e95c524b7da792
siitp <reg.lo@yandex.ru>
2017-01-22 19:52:10 +0300

    set result in description if exists

--- app/src/main/java/ru/mail/sporttogether/fragments/EventsMapFragment.kt
+++ app/src/main/java/ru/mail/sporttogether/fragments/EventsMapFragment.kt
@@ -22,7 +22,6 @@ import com.google.android.gms.maps.MapView
 import ru.mail.sporttogether.R
 import ru.mail.sporttogether.activities.AddEventActivity
 import ru.mail.sporttogether.activities.DrawerActivity
-import ru.mail.sporttogether.mvp.PresenterActivity
 import ru.mail.sporttogether.adapter.TaskAdapter
 import ru.mail.sporttogether.adapter.events.EventsAdapter
 import ru.mail.sporttogether.app.App
@@ -34,6 +33,7 @@ import ru.mail.sporttogether.databinding.EventsMapBinding
 import ru.mail.sporttogether.fragments.presenter.EventsMapFragmentPresenter
 import ru.mail.sporttogether.fragments.presenter.EventsMapFragmentPresenterImpl
 import ru.mail.sporttogether.fragments.view.EventsMapView
+import ru.mail.sporttogether.mvp.PresenterActivity
 import ru.mail.sporttogether.mvp.PresenterFragment
 import ru.mail.sporttogether.net.models.Event
 import ru.mail.sporttogether.net.models.Task
@@ -322,7 +322,7 @@ class EventsMapFragment :
         data.address.set(event.address)
         data.name.set(event.name)
         data.date.set(DateUtils.toXLongDateString(Date(event.date)))
-        data.description.set(event.description)
+        data.description.set(event.result ?: event.description)
         data.withDescription.set(event.description.isNotEmpty())
         data.isReported.set(event.isReported)
         data.isJoined.set(event.isJoined)
