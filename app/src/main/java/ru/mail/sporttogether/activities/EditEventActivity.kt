e3588229d9a3df633da36237507a309a3f066de7
Vladislav Kozhushko <bagrusss@gmail.com>
2017-01-23 16:37:22 +0300

    Minimal edit event

--- app/src/main/java/ru/mail/sporttogether/activities/EditEventActivity.kt
+++ app/src/main/java/ru/mail/sporttogether/activities/EditEventActivity.kt
@@ -4,11 +4,12 @@ import android.content.Context
 import android.content.Intent
 import android.databinding.DataBindingUtil
 import android.os.Bundle
+import com.mikepenz.materialize.util.KeyboardUtil
 import ru.mail.sporttogether.R
 import ru.mail.sporttogether.activities.presenter.EditEventActivityPresenter
 import ru.mail.sporttogether.activities.presenter.EditEventActivityPresenterImp
 import ru.mail.sporttogether.activities.view.EditEventView
-import ru.mail.sporttogether.databinding.ActivityEditEventBinding
+import ru.mail.sporttogether.databinding.ActivityEditEventMinimalBinding
 import ru.mail.sporttogether.mvp.PresenterActivity
 import ru.mail.sporttogether.net.models.Event
 
@@ -16,14 +17,19 @@ class EditEventActivity :
         PresenterActivity<EditEventActivityPresenter>(),
         EditEventView {
 
-    private lateinit var binding: ActivityEditEventBinding
+    private lateinit var binding: ActivityEditEventMinimalBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_edit_event)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_edit_event_minimal)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         presenter = EditEventActivityPresenterImp(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        KeyboardUtil.hideKeyboard(this)
     }
 
     companion object {
