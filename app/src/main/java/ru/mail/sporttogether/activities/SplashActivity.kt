c175d8dbba7117263d5a844a1909eb86687a21ca
Vladislav Kozhushko <bagrusss@gmail.com>
2017-02-05 18:59:21 +0300

    Redesign splash activity as material pattern

--- app/src/main/java/ru/mail/sporttogether/activities/SplashActivity.kt
+++ app/src/main/java/ru/mail/sporttogether/activities/SplashActivity.kt
@@ -3,6 +3,7 @@ package ru.mail.sporttogether.activities
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
@@ -10,6 +11,8 @@ import ru.mail.sporttogether.R
 import ru.mail.sporttogether.activities.presenter.SplashActivityPresenter
 import ru.mail.sporttogether.activities.presenter.SplashActivityPresenterImpl
 import ru.mail.sporttogether.activities.view.SplashView
+import ru.mail.sporttogether.data.binding.SplashData
+import ru.mail.sporttogether.databinding.ActivitySplashBinding
 import ru.mail.sporttogether.mvp.PresenterActivity
 
 class SplashActivity :
@@ -17,6 +20,8 @@ class SplashActivity :
         SplashView {
 
     private var bundle: Bundle? = null
+    private val data = SplashData()
+    private lateinit var binding: ActivitySplashBinding
 
     override fun getActivity(): Activity {
         return this
@@ -24,7 +29,9 @@ class SplashActivity :
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
+        binding.data = data
+        data.text.set(getString(R.string.app_name).toUpperCase())
         presenter = SplashActivityPresenterImpl(this)
         presenter.onCreate(savedInstanceState)
         bundle = intent.getBundleExtra("data")
---
+++ app/src/main/java/ru/mail/sporttogether/data/binding/SplashData.kt
@@ -0,0 +1,10 @@
+package ru.mail.sporttogether.data.binding
+
+import android.databinding.ObservableField
+
+/**
+ * Created by bagrusss on 05.02.17
+ */
+class SplashData {
+    val text = ObservableField<String>()
+}
