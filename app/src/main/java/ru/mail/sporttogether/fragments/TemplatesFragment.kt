6d8273e68da394c0d39de3a9cc827a0cc5e63fae
Vladislav Kozhushko <bagrusss@gmail.com>
2017-02-07 01:06:07 +0300

    Refactor

--- app/src/main/java/ru/mail/sporttogether/fragments/TemplatesFragment.kt
+++ app/src/main/java/ru/mail/sporttogether/fragments/TemplatesFragment.kt
@@ -1,9 +1,12 @@
 package ru.mail.sporttogether.fragments
 
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import ru.mail.sporttogether.R
+import ru.mail.sporttogether.data.binding.ListLayoutData
 import ru.mail.sporttogether.databinding.LayoutListBinding
 import ru.mail.sporttogether.fragments.presenter.TemplatesPresenter
 import ru.mail.sporttogether.fragments.presenter.TemplatesPresenterImpl
@@ -16,9 +19,13 @@ import ru.mail.sporttogether.mvp.PresenterFragment
 class TemplatesFragment : PresenterFragment<TemplatesPresenter>(), TemplatesView {
 
     protected lateinit var binding: LayoutListBinding
+    private val data = ListLayoutData()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = LayoutListBinding.inflate(inflater, container, false)
+        binding.data = data
+        data.emptyDrawable.set(ContextCompat.getDrawable(context, R.drawable.ic_templates))
+        data.emptyText.set(getString(R.string.empty))
         presenter = TemplatesPresenterImpl()
         return binding.root
     }
