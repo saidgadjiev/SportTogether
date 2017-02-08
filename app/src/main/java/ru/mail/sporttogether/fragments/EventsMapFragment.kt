30b7cc21ce7c9b2b8dd587c5fdb8d1ff4ed52290
siitp <reg.lo@yandex.ru>
2017-02-08 21:59:56 +0300

    optimize animations

--- app/src/main/java/ru/mail/sporttogether/fragments/EventsMapFragment.kt
+++ app/src/main/java/ru/mail/sporttogether/fragments/EventsMapFragment.kt
@@ -447,16 +447,20 @@ class EventsMapFragment :
 
     override fun showEventsList() {
         val mapEventsLayout = binding.mapEventsListInclude.mapEventsLayout
-        mapEventsLayout.visibility = View.VISIBLE
-        mapEventsLayout.animate().scaleX(1f).setDuration(300L).start()
+        if (mapEventsLayout.visibility != View.VISIBLE) {
+            mapEventsLayout.visibility = View.VISIBLE
+            mapEventsLayout.animate().scaleX(1f).setDuration(300L).start()
+        }
 
     }
 
     override fun hideEventsList() {
         val mapEventsLayout = binding.mapEventsListInclude.mapEventsLayout
-        mapEventsLayout.animate().scaleX(0f).setDuration(300L).withEndAction {
-            mapEventsLayout.visibility = View.GONE
-        }.start()
+        if (mapEventsLayout.visibility != View.GONE) {
+            mapEventsLayout.animate().scaleX(0f).setDuration(300L).withEndAction {
+                mapEventsLayout.visibility = View.GONE
+            }.start()
+        }
     }
 
     override fun onLocationNotChecked() {
